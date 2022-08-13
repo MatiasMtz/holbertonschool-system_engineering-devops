1fdef67c289c9163ef572913b8d6f791161b3a1b
MatiasMtz <4442@holbertonschool.com>
2022-08-13 02:19:54 +0000

    create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
-# creates a file in /tmp
-file {'/tmp/school':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',}
+# Create a file in /tmp.
+
+file { '/tmp/school':
+    ensure  => 'file',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
