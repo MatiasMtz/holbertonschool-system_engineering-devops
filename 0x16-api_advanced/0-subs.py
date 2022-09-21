f1ab8163e0a427dbab3bbdf9ff22614b009c839f
MatiasMtz <4442@holbertonschool.com>
2022-09-21 19:31:11 +0000

    fixed pycode

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of 
+"""function that queries the Reddit API and returns the number of
 subscribers or 0"""
 import requests
 
